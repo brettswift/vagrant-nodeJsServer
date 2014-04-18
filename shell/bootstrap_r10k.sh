9462ffb6602c5d2a88b68b6e7ffc015bae643cab
Brett Swift <brettswift@gmail.com>
2014-04-18 10:01:21 -0600

    enabling git install for r10k

--- shell/bootstrap_r10k.sh
+++ shell/bootstrap_r10k.sh
@@ -5,18 +5,18 @@ PUPPET_DIR='/vagrant'
 
   echo "running r10k bootstrap."
 
-# GIT=/usr/bin/git
-# APT_GET=/usr/bin/apt-get
-# YUM=/usr/bin/yum
-# if [ ! -x $GIT ]; then
-#     if [ -x $YUM ]; then
-#         yum -q -y install git-core
-#     elif [ -x $APT_GET ]; then
-#         apt-get -q -y install git-core
-#     else
-#         echo "No package installer available. You may need to install git manually."
-#     fi
-# fi
+GIT=/usr/bin/git
+APT_GET=/usr/bin/apt-get
+YUM=/usr/bin/yum
+if [ ! -x $GIT ]; then
+    if [ -x $YUM ]; then
+        yum -q -y install git-core
+    elif [ -x $APT_GET ]; then
+        apt-get -q -y install git-core
+    else
+        echo "No package installer available. You may need to install git manually."
+    fi
+fi
 
 # gem list <name> -i will return the string true if the gem is installed and false otherwise. Also, the return codes are what you would expect.
 if [ `gem list r10k -i` ]; then
